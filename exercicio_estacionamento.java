96f99b690b1eeac9c776b91fac6a5d115074db6c
Vgon0903 <103973688+Vgon0903@users.noreply.github.com>
2022-05-23 06:23:00 -0300

    Update exercicio_estacionamento.java

--- exercicio_estacionamento.java
+++ exercicio_estacionamento.java
@@ -5,29 +5,37 @@ public class exercicio_estacionamento {
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		int condicao = 0;
+		//variaveis criadas
+		int condicao = 0; 
 		int deficiente,gestante,idoso;
+		//Exibir mensagem e quebrar linha
 		System.out.printf("Vaga preferencial \n");
+		//Exibir mensagem para escolher uma das opcoes apresentadas
 		System.out.printf("Para estacionar digite: (1) Deficiente (2)Gestante (3)Idoso (4)Se voce nao se identifica com as opcoes anteriores");
-		condicao = sc.nextInt();
+		condicao = sc.nextInt(); // ler opcao e armazenar na variavel "condicao"
 		
-		switch(condicao)
+		switch(condicao) //estrutura de decisao
 		{
-		case 1: 
+		case 1: //caso escolha a opcao 1 ira exibir a mensagem a baixo
 			System.out.printf("Voce se identificou como Deficiente \n Estacionamento liberado");
-			break;
-		case 2: 
+			//para laco de repeticao	
+			break; 
+		case 2:	//caso escolha a opcao 2 ira exibir a mensagem a baixo	
 			System.out.printf("Voce se identificou como Gestante \n Estacionamento liberado");
+			//para laco de repeticao
 			break;	
-		case 3: 
+		case 3: //caso escolha a opcao 3 ira exibir a mensagem a baixo	
 			System.out.printf("Voce se identificou como Idoso \n Estacionamento liberado");
-			break;
-		case 4: 
+			//para laco de repeticao	
+			break; 
+		case 4: //caso escolha a opcao 4 ira exibir a mensagem a baixo	
 			System.out.printf("Voce nao possui nenhuma das necessidades exigidas para estacionar \n Estacionamento NAO liberado");
-			break;		
-		default:
+			//para laco de repeticao	
+			break;	
+		default: //caso nao escolha nenhuma das opcoes apresentadas ira exibir a mensagem a baixo	
 			System.out.printf("ERRO, favor escolher uma das opcoes apresentadas");
-			break;
+			//para laco de repeticao	
+			break; 
 		
 
 	}
